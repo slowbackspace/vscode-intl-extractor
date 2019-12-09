a5709932036493447d14408162dbe811b31d23fb
slowbackspace <slowbackspace@gmail.com>
2019-12-09 23:39:08 +0100

    fix setting default value for boolean config entry

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,8 @@ const ignoreTokens = config.get<string[]>('ignoreTokens') || ['a', 'an', 'the', 
 const messageComponent = config.get<string>('messageComponent') || 'FormattedMessage';
 const messagesFilePath = config.get<string>('messagesFilePath');
 const messageIdPrefix = config.get<string>('messageIdPrefix') || 'TR';
-const trailingComma = config.get<boolean>('trailingComma') || true;
+let trailingComma = config.get<boolean>('trailingComma');
+trailingComma = trailingComma === undefined ? true : trailingComma;
 
 export default {
     ignoreTokens,
