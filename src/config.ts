1a54eb33db4c8c976a5b8495bcbc784d0ec72ccf
slowbackspace <slowbackspace@gmail.com>
2020-03-25 10:41:22 +0100

    add ignore tokens

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 const config = vscode.workspace.getConfiguration('intlExtractor');
 
-const ignoreTokens = config.get<string[]>('ignoreTokens') || ['a', 'an', 'the', '.', ','];
+const ignoreTokens = config.get<string[]>('ignoreTokens') || ['a', 'an', 'the', '.', ',', '&'];
 const messageComponent = config.get<string>('messageComponent') || 'FormattedMessage';
 const messagesFilePath = config.get<string>('messagesFilePath');
 const messageIdPrefix = config.get<string>('messageIdPrefix') || 'TR';
